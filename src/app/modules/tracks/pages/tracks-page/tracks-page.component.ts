e4c1972f14386d77fa69875d36f5cbef670e6554
TorresMBA <bryan98tm@gmail.com>
2022-09-18 00:24:44 -0500

    Se agrego un path alias en el config.json.ts, se añadió componentes de pagina a cada uno de los modulos y se agrego nuevo router outlet para el home

---
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-history-page',
+  templateUrl: './tracks-page.component.html',
+  styleUrls: ['./tracks-page.component.css']
+})
+export class TracksPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
